dccf122fe0dab6015feb857d693ff5fb152e0879
double_s <sz.suchodolski@gmail.com>
2021-03-16 20:33:15 +0100

    add test comment

--- src/test/Dummy.test.js
+++ src/test/Dummy.test.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+//first tests
 
 const InputWithButton = () => {
   const [inputValue, setInputValue] = useState('');
