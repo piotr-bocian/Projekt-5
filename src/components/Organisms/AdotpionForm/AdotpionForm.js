d0d4601eb59ea8719375e2d5be6e364937952e2f
Your Name <you@example.com>
2021-03-22 20:02:36 +0100

    added Adoption Form Component

--- src/components/Organisms/AdotpionForm/AdotpionForm.js
+++ src/components/Organisms/AdotpionForm/AdotpionForm.js
@@ -7,29 +7,39 @@ import './AdotpionForm.css';
 const AdotpionForm = () => {
     const [title, setTitle] = useState('');
     const [question, setQuestion] = useState('');
+    const animalName = "Rudy"
 
     const sendForm = (e) => {
         e.preventDefault();
         console.log(title);
         console.log(question);
     }
-    
+
     return (
         <form className="form" onSubmit={sendForm}>
-            <Grid container spacing={3}>
+            <Grid container spacing={3} justify="center">
                 <Grid item xs={12} sm={12} md={12}>
                     <Typography
                         variant="h4"
                         align="center"
                         color="textPrimary"
                     >
-                        Zadaj pytanie odnośnie zwierzaka
+                    Zadaj pytanie
                     </Typography>
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12}>
+                    <TextField
+                        variant="outlined"
+                        disabled
+                        label="Imię zwierzaka*" value={animalName}
+                        fullWidth
+                    />
                 </Grid>
                 <Grid item xs={12} sm={12} md={12}>
                     <TextField
                         variant="outlined"
-                        label="Tytuł" value={title}
+                        label="Tytuł *" value={title}
                         onChange={(e) => setTitle(e.target.value)}
                         fullWidth
                     />
@@ -37,21 +47,21 @@ const AdotpionForm = () => {
                 <Grid item xs={12}>
                     <TextField
                         variant="outlined"
-                        label="Zadaj pytanie"
+                        label="Treść *"
                         multiline
                         rows={6}
                         value={question} onChange={(e) => setQuestion(e.target.value)}
                         fullWidth
                     />
                 </Grid>
-                <Grid item xs={12}>
+                <Grid item xs={4}>
                     <Button
                         color="primary"
                         variant="contained"
                         type="submit"
                         fullWidth
                     >
-                        Wyślij
+                    Wyślij
                     </Button>
                 </Grid>
             </Grid>
