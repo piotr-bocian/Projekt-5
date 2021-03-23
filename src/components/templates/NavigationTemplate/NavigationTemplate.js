6fccb47f5a7c7951c19b3cd40e30524c7558a944
Łukasz Żurawski <zurawskiu@gmail.com>
2021-03-23 18:20:16 +0100

    remove Hello World component

--- src/components/templates/NavigationTemplate/NavigationTemplate.js
+++ src/components/templates/NavigationTemplate/NavigationTemplate.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../../../styles/styles';
-import { Typography } from '@material-ui/core';
-import AppleIcon from '@material-ui/icons/Apple';
 import Navigation from '../../Organisms/Navigation/Navigation';
 import GridContainerNavigationWrapper from '../../../styles/stylesContainer/GridContainerNavigationWrapper';
 import PaymentForm from '../../Organisms/PaymentForm/PaymentForm';
@@ -36,49 +34,47 @@ function NavigationView() {
     <ThemeProvider theme={theme}>
       <Router>
         <NavigationTemplate>
-          <Wrapper>
-            <Switch>
-              <Route path="/" exact>
-                {/* odpowiedni komponent */}
-              </Route>
-            </Switch>
-            <Switch>
-              <Route path="/registration"></Route>
-            </Switch>
-            <Switch>
-              <Route path="/login"></Route>
-            </Switch>
-            <Switch>
-              <Route path="/news"></Route>
-            </Switch>
-            <Switch>
-              <Route path="/animals">
-                <Animals />
-              </Route>
-            </Switch>
-            <Switch>
-              <Route path="/adoption">
-                  <VisitForm />
-              </Route>
-            </Switch>
-            <Switch>
-              <Route path="/education"></Route>
-            </Switch>
-            <Switch>
-              <Route path="/howtohelp">
-                <PaymentForm />
-              </Route>
-            </Switch>
-            <Switch>
-              <Route path="/about">
-                  <Typography variant="h1">Hello world!</Typography>
-                  <AppleIcon style={{ fontSize: 130 }} />
-              </Route>
-            </Switch>
-            <Switch>
-              <Route path="/contact"></Route>
-            </Switch>
-          </Wrapper>
+          <Switch>
+            <Route path="/" exact>
+              {/* odpowiedni komponent */}
+            </Route>
+          </Switch>
+          <Switch>
+            <Route path="/registration"></Route>
+          </Switch>
+          <Switch>
+            <Route path="/login"></Route>
+          </Switch>
+          <Switch>
+            <Route path="/news"></Route>
+          </Switch>
+          <Switch>
+            <Route path="/animals">
+              <Animals />
+            </Route>
+          </Switch>
+          <Switch>
+            <Route path="/adoption">
+              <Wrapper>
+                <VisitForm />
+              </Wrapper>
+            </Route>
+          </Switch>
+          <Switch>
+            <Route path="/education"></Route>
+          </Switch>
+          <Switch>
+            <Route path="/howtohelp">
+              <PaymentForm />
+            </Route>
+          </Switch>
+          <Switch>
+            <Route path="/about">
+            </Route>
+          </Switch>
+          <Switch>
+            <Route path="/contact"></Route>
+          </Switch>
         </NavigationTemplate>
       </Router>
     </ThemeProvider>
--- src/view/App.js
+++ src/view/App.js
@@ -1,13 +1,5 @@
 import React from 'react';
-
-import styled from 'styled-components';
 import NavigationView from '../components/templates/NavigationTemplate/NavigationTemplate';
-
-
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-`;
 
 function App() {
   return (
