e22fa38bb874cbc922c251783de4843b37392504
Jan Eliasz <johnbg@tlen.pl>
2021-03-23 21:17:16 +0100

    Create VolunteerFormTextField component

--- src/components/Organisms/VolunteerForm/VolunteerForm.js
+++ src/components/Organisms/VolunteerForm/VolunteerForm.js
@@ -1,7 +1,8 @@
-import React, { useState, useRef } from 'react';
-import { TextField, RadioGroup, FormControlLabel, Radio, Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import { TextField, RadioGroup, FormControlLabel, Radio, Button, FormControl, FormLabel } from '@material-ui/core';
 import { Grid } from '@material-ui/core';
 import useHttp from '../../../hooks/useHttp/useHttp';
+import VolunteerFormTextField from '../../Atoms/VolunteerFormTextField/VolunteerFormTextField';
 
 import './VolunteerForm.css';
 
@@ -17,6 +18,10 @@ const VolunteerForm = () => {
 
     const [firstNameErr, setFirstNameErr] = useState(false);
     const [lastNameErr, setLastNameErr] = useState(false);
+    const [mobileErr, setMobileErr] = useState(false);
+    const [birthDateErr, setBirthDateErr] = useState(false);
+    const [occupationErr, setOccupationErr] = useState(false);
+    const [preferredTasksErr, setPreferredTasksErr] = useState(false);
 
     const updateForm = (e) => {
         setForm({
@@ -25,7 +30,7 @@ const VolunteerForm = () => {
         })
     }
 
-    const validateForm = () => {
+    const validateForm = () => { //this function returns true if all fields are correct, otherwise sets the errors and returns false
         let valid = true;
 
         if(!form.firstName){
@@ -34,6 +39,22 @@ const VolunteerForm = () => {
         }
         if(!form.lastName){
             setLastNameErr(true);
+            valid = false;
+        }
+        if(form.mobile.length !== 9){
+            setMobileErr(true);
+            valid = false;
+        }
+        if(!form.birthDate){
+            setBirthDateErr(true);
+            valid = false;
+        }
+        if(!form.occupation){
+            setOccupationErr(true);
+            valid = false;
+        }
+        if(!form.preferredTasks){
+            setPreferredTasksErr(true);
             valid = false;
         }
 
@@ -59,7 +80,7 @@ const VolunteerForm = () => {
         <form className="form" onSubmit={handleSubmit}>
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={6}>
-                    <TextField 
+                    {/* <TextField 
                         variant="outlined" 
                         label="Imię"
                         name="firstName"
@@ -72,10 +93,18 @@ const VolunteerForm = () => {
                         }
                         fullWidth
                         error={firstNameErr}
+                    /> */}
+                    <VolunteerFormTextField 
+                        label="Imię" 
+                        name="firstName" 
+                        value={form.firstName} 
+                        updateForm={updateForm} 
+                        err={firstNameErr} 
+                        setErr={setFirstNameErr}
                     />
                 </Grid>
                 <Grid item xs={12} sm={6}>    
-                    <TextField 
+                    {/* <TextField 
                         variant="outlined" 
                         label="Nazwisko"
                         name="lastName"
@@ -88,16 +117,38 @@ const VolunteerForm = () => {
                         } 
                         fullWidth
                         error={lastNameErr}
+                    /> */}
+                    <VolunteerFormTextField 
+                        label="Nazwisko" 
+                        name="lastName" 
+                        value={form.lastName} 
+                        updateForm={updateForm} 
+                        err={lastNameErr} 
+                        setErr={setLastNameErr}
                     />
                 </Grid>
                 <Grid item xs={12} sm={6}>    
-                    <TextField 
+                    {/* <TextField 
                         variant="outlined"
                         label="Telefon"
                         name="mobile" 
                         value={form.mobile} 
-                        onChange={updateForm} 
+                        onChange={
+                            (e) => {
+                                setMobileErr(false);
+                                updateForm(e);
+                            }
+                        }
                         fullWidth
+                        error={mobileErr}
+                    /> */}
+                    <VolunteerFormTextField 
+                        label="Telefon" 
+                        name="mobile" 
+                        value={form.mobile} 
+                        updateForm={updateForm} 
+                        err={mobileErr} 
+                        setErr={setMobileErr}
                     />
                 </Grid>
                 <Grid item xs={12} sm={6}>
@@ -109,9 +160,15 @@ const VolunteerForm = () => {
                         }} 
                         name="birthDate"
                         value={form.birthDate} 
-                        onChange={updateForm} 
+                        onChange={
+                            (e) => {
+                                setBirthDateErr(false);
+                                updateForm(e);
+                            }
+                        }
                         type="date"
-                        fullWidth 
+                        fullWidth
+                        error={birthDateErr}
                     />
                 </Grid>
                 <Grid item xs={12}>    
@@ -122,29 +179,49 @@ const VolunteerForm = () => {
                         rows={4}
                         name="occupation"
                         value={form.occupation} 
-                        onChange={updateForm} 
+                        onChange={
+                            (e) => {
+                                setOccupationErr(false);
+                                updateForm(e);
+                            }
+                        }
                         fullWidth
+                        error={occupationErr}
                     />
                 </Grid>
                 
                 <Grid item xs={12}>
-                    <RadioGroup row name="preferredTasks" value={form.preferredTasks} onChange={updateForm}>
-                        <FormControlLabel 
-                            value="praca z psami" 
+                    <FormControl component="fieldset" error={preferredTasksErr}>
+                        <FormLabel component="legend">Preferowane zadanie w schronisku</FormLabel>
+                        <RadioGroup 
+                            row 
+                            name="preferredTasks" 
+                            value={form.preferredTasks} 
+                            onChange={
+                                (e) => 
+                                    {
+                                        setPreferredTasksErr(false);
+                                        updateForm(e);
+                                    }
+                                }           
+                        >
+                            <FormControlLabel 
+                                value="praca z psami" 
+                                control={<Radio color="primary" disableRipple />} 
+                                label="Praca z psami" 
+                            />
+                            <FormControlLabel 
+                                value="praca z kotami" 
+                                control={<Radio color="primary" disableRipple />}
+                                label="Praca z kotami"
+                            />
+                            <FormControlLabel 
+                            value="promocja schroniska" 
                             control={<Radio color="primary" disableRipple />} 
-                            label="Praca z psami" 
-                        />
-                        <FormControlLabel 
-                            value="praca z kotami" 
-                            control={<Radio color="primary" disableRipple />}
-                            label="Praca z kotami"
-                        />
-                        <FormControlLabel 
-                        value="promocja schroniska" 
-                        control={<Radio color="primary" disableRipple />} 
-                        label="Promocja schroniska"
-                        />
-                    </RadioGroup>
+                            label="Promocja schroniska"
+                            />
+                        </RadioGroup>
+                    </FormControl>
                 </Grid>
                 
                 <Grid item xs={12}>
