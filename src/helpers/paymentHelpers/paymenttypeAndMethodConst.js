55a1ce99d1c65043b1b588754b1010a787442c86
Łukasz Żurawski <zurawskiu@gmail.com>
2021-03-26 15:17:43 +0100

    prevent to send empty or invalid data

--- src/helpers/paymentHelpers/paymenttypeAndMethodConst.js
+++ src/helpers/paymentHelpers/paymenttypeAndMethodConst.js
@@ -1,11 +1,11 @@
 export const typeOfPayment = [
-  'Wybierz typ płatności',
+  '',
   'Opłata adopcyjna',
   'Jednorazowy przelew',
   'Wirtualny opiekun-opłata cykliczna',
 ];
 export const paymentMethod = [
-  'Wybierz metodę płatności',
+  '',
   'Karta płatnicza',
   'Blik',
   'Przelew bankowy',
