b00a29eb5d6ad006eb827c32b7fa2747769d7206
double_s <sz.suchodolski@gmail.com>
2021-03-27 00:01:06 +0100

    corrected App.js as it is on gitHub

--- src/view/App.js
+++ src/view/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
-import styled from 'styled-components';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core';
-
-=======
 import { ThemeProvider, createMuiTheme } from '@material-ui/core';
 import NavigationView from '../components/templates/NavigationTemplate/NavigationTemplate';
->>>>>>> development
 
 const theme = createMuiTheme({
   palette: {
@@ -18,28 +12,11 @@ const theme = createMuiTheme({
   },
 });
 
-const theme = createMuiTheme({
-  palette:{
-    primary:{
-      light: '#0292c9',
-      main: '#0093CA',
-      dark: '#0274A2'
-    }
-  }
-});
-
 function App() {
   return (
-<<<<<<< HEAD
-    <Wrapper>
-      <ThemeProvider theme={theme}>
-      </ThemeProvider>
-    </Wrapper>
-=======
     <ThemeProvider theme={theme}>
       <NavigationView />
     </ThemeProvider>
->>>>>>> development
   );
 }
 
