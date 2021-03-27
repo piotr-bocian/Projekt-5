6331c37b5c4717c44a2ef0dd6f0ebb27552ad338
Łukasz Żurawski <zurawskiu@gmail.com>
2021-03-27 14:51:44 +0100

    added error handler to select

--- src/components/Atoms/Select/Select.js
+++ src/components/Atoms/Select/Select.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import { Label } from '../Label/Label';
 import { Wrapper } from '../../Molecules/PaymentFormField/PaymentFormFieldWrapper.styles';
-import { NativeSelect } from '@material-ui/core';
+import { FormControl, NativeSelect } from '@material-ui/core';
 import FormHelperText from '@material-ui/core/FormHelperText';
 
 const SelectPay = ({ onChange, optionType, name, id, label, error }) => {
   return (
     <Wrapper>
-      <Label htmlFor={id}>
-        {label}
-        <NativeSelect
-          style={{ display: 'block', marginTop: '10px' }}
-          id={id}
-          name={name}
-          onChange={onChange}
-        >
-          {optionType.map((type, index) => (
-            <option key={index} value={type}>
-              {type}
-            </option>
-          ))}
-        </NativeSelect>
-        <FormHelperText style={{ color: 'red' }}>{error}</FormHelperText>
-      </Label>
+      <FormControl error={error}>
+        <Label htmlFor={id}>
+          {label}
+          <NativeSelect
+            style={{ display: 'block', marginTop: '10px' }}
+            id={id}
+            name={name}
+            onChange={onChange}
+          >
+            {optionType.map((type, index) => (
+              <option key={index} value={type}>
+                {type}
+              </option>
+            ))}
+          </NativeSelect>
+          <FormHelperText style={{ color: 'red' }}>{error}</FormHelperText>
+        </Label>
+      </FormControl>
     </Wrapper>
   );
 };
