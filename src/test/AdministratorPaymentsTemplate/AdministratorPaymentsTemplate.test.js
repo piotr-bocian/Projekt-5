de42cee151a3fa7a0b15deef71fd7c3664aca502
Łukasz Żurawski <zurawskiu@gmail.com>
2021-03-30 20:50:46 +0200

    Move AdministratorPaymentsTemplate to test folder, remove usereEvent, add fireEvent in test

--- src/test/AdministratorPaymentsTemplate/AdministratorPaymentsTemplate.test.js
+++ src/test/AdministratorPaymentsTemplate/AdministratorPaymentsTemplate.test.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { render, fireEvent, waitFor, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 
-import AdministratorPaymentsTemplate from './AdministratorPaymentsTemplate';
+import AdministratorPaymentsTemplate from '../../components/templates/AdministratorPaymentsTemplate/AdministratorPaymentsTemplate';
 
 const fetchedPaymentsArray = {
   payments: {
@@ -39,6 +38,7 @@ describe('Is Administrator Payments View with data fetched from API correct rend
     await waitFor(() => screen.getByText(/Id płatności:/i));
     await waitFor(() => screen.getByText(/Data płatności:/i));
     expect(screen.getByText(/Kwota/i)).toBeInTheDocument();
+    expect(screen.getByDisplayValue(/34/i)).toBeInTheDocument();
     expect(screen.getByText(/2021-03-28/i)).toBeInTheDocument();
     expect(screen.getByText(/60609e9058b9550015ffe8d4/i)).toBeInTheDocument();
     expect(screen.getByText(/Id płatności:/i)).toBeInTheDocument();
@@ -48,19 +48,15 @@ describe('Is Administrator Payments View with data fetched from API correct rend
   it('should display menu with options after user left click "Filtr metoda płatności"', async () => {
     render(<AdministratorPaymentsTemplate />);
     await waitFor(() => screen.getByText('Filtr metoda płatności'));
-    const leftClick = { button: 0 };
-    userEvent.click(screen.getByText(/Filtr metoda płatności/i), leftClick);
+    fireEvent.click(screen.getByText(/Filtr metoda płatności/i));
     expect(screen.getByText(/Blik/i)).toBeInTheDocument();
   });
 
   it('should display menu with options after user left click on "Filtr Typ płatności"', async () => {
     render(<AdministratorPaymentsTemplate />);
     await waitFor(() => screen.getByText('Filtr typ płatności'));
-    const leftClick = { button: 0 };
-    userEvent.click(screen.getByText(/Filtr typ płatności/i), leftClick);
-    expect(
-      screen.getByText(/Wirtualny opiekun-opłata cykliczna/i)
-    ).toBeInTheDocument();
+    fireEvent.click(screen.getByText(/Filtr typ płatności/i));
+    expect(screen.getByText(/opłata adopcyjna/i)).toBeInTheDocument();
   });
 
   it('should display correct buttons', async () => {
