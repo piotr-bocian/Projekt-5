bf207f917266bd21f5d48a0c1c3390959ebd2c84
daria977 <72752181+daria977@users.noreply.github.com>
2021-04-14 22:06:47 +0200

    fixed visit button

--- src/components/Organisms/AdoptionVisit/AdoptionVisitForm.js
+++ src/components/Organisms/AdoptionVisit/AdoptionVisitForm.js
@@ -40,6 +40,7 @@ const durationValues = [
 ];
 
 const VisitForm = ({ animal }) => {
+    const { isLogged } = useAuth();
 
     const [visitDate, setVisitDate] = useState(minDate);
     const [visitTime, setVisitTime] = useState("09:00");
@@ -118,7 +119,7 @@ const VisitForm = ({ animal }) => {
 
     return  (
     <div>
-        <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        <Button variant="outlined" color="primary" onClick={handleClickOpen} disabled={!isLogged}>
             Zarezerwuj wizytę
         </Button>
 
